7f875e56d5641a3865c9bd61a664ca49afa5d5c3
Ciprian Ciubotariu <ciprian.ciubotariu@digital.justice.gov.uk>
2015-05-06 13:43:36 +0100

    add rattic.service.dsd.io to the allowed hosts

--- files/init_and_gunicorn.sh
+++ files/init_and_gunicorn.sh
@@ -7,7 +7,7 @@ if [ ! -f ${RATTIC_CONFIG_PATH} ]; then
   sed "s/%SECRETKEY%/$(openssl rand -base64 48 | cut -c1-32 | tr '/' '-')/g" /opt/local.dist.cfg > ${RATTIC_CONFIG_PATH}
   cd /opt/rattic/ && ./manage.py syncdb --noinput && ./manage.py migrate --all
   echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'rattic')" | ./manage.py shell
-  echo "ALLOWED_HOSTS = ['127.0.0.1', 'localhost']" >> /opt/rattic/ratticweb/settings.py
+  echo "ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rattic.service.dsd.io']" >> /opt/rattic/ratticweb/settings.py
 fi
 
 gunicorn ratticweb.wsgi -b 127.0.0.1:8000 -k gevent --log-config logging.config
